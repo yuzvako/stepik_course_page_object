f62e2fed543e9d7822f5d8c45ef52420c25de9ea
Nikita Yuzva <yuzva71@mail.ru>
2023-03-01 16:19:46 +0200

    PyTest main test for Page Object

--- test_main_page.py
+++ test_main_page.py
@@ -1,7 +1,12 @@
 from selenium.webdriver.common.by import By
 import time
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
+
+def go_to_login_page(browser):
     login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
     login_link.click()
+
+link = "http://selenium1py.pythonanywhere.com/"
+
+def test_guest_can_go_to_login_page(browser):
+    browser.get(link)
+    go_to_login_page(browser)
