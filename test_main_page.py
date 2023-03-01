9f60c662f75b494bfdddc0499de9fb1ece2b1c68
Nikita Yuzva <yuzva71@mail.ru>
2023-03-01 22:02:44 +0200

    add login-registration form test

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,5 @@
-from selenium.webdriver.common.by import By
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
@@ -12,3 +12,12 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+def test_login_page_locators(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.open()
+    login_page.should_be_login_page()
