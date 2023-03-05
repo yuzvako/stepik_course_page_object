c7316375292ba5a6790c5184fd661b2abdd7f082
Nikita Yuzva <yuzva71@mail.ru>
2023-03-05 14:34:08 +0200

    Add registration new user function

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,6 +1,6 @@
 from .base_page import BasePage
 from .locators import LoginPageLocators
-
+import time
 
 class LoginPage(BasePage):
     def should_be_login_page(self):
@@ -16,3 +16,14 @@ class LoginPage(BasePage):
 
     def should_be_register_form(self):
         assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not present"
+
+    def register_new_user(self, email, password):
+        login_field = self.browser.find_element(*LoginPageLocators.LOGIN_FIELD)
+        login_field.send_keys(email)
+        password_field = self.browser.find_element(*LoginPageLocators.PASSWORD_FIELD)
+        password_field.send_keys(password)
+        password_submit = self.browser.find_element(*LoginPageLocators.PASSWORD_FIELD_SUBMIT)
+        password_submit.send_keys(password)
+        submit_button = self.browser.find_element(*LoginPageLocators.SUBMIT_BUTTON)
+        submit_button.click()
+
