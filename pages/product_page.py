dae9490da089dc2f901c8ce778ab7a2170adeece
Nikita Yuzva <yuzva71@mail.ru>
2023-03-05 03:18:18 +0200

    Add success message tests

--- pages/product_page.py
+++ pages/product_page.py
@@ -1,6 +1,11 @@
 from .base_page import BasePage
 from .locators import ProductPageLocators
 class ProductPage(BasePage):
+    def add_to_basket(self):
+        add_to_basket = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET)
+        add_to_basket.click()
+        self.solve_quiz_and_get_code()
+        assert self.is_element_present(*ProductPageLocators.ADD_TO_BASKET), "Add to basket not found"
     def should_add_book_to_basket(self):
         add_to_basket = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET)
         add_to_basket.click()
@@ -18,3 +23,11 @@ class ProductPage(BasePage):
         self.book_price = self.browser.find_element(*ProductPageLocators.PRICE_BOOK).text[1:]
         self.basket = self.browser.find_element(*ProductPageLocators.BASKET_PRICE).text[1:]
         assert self.book_price == self.basket, "Prices is not equal"
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be"
+
+    def should_not_be_success_message_is_disappeared(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be"
