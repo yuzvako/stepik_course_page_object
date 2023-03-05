94b849ae62dc4bc5adeeab6c695fd1721a8144a3
Nikita Yuzva <yuzva71@mail.ru>
2023-03-05 02:05:36 +0200

    Add Product page locators

--- pages/locators.py
+++ pages/locators.py
@@ -9,3 +9,7 @@ class LoginPageLocators():
 
 class ProductPageLocators():
     ADD_TO_BASKET = (By.CLASS_NAME, "btn-add-to-basket")
+    BOOK_NAME = (By.XPATH, '/html/body/div[2]/div/div[2]/div[2]/article/div[1]/div[2]/h1')
+    PRICE_BOOK = (By.XPATH, '/html/body/div[2]/div/div[2]/div[2]/article/div[1]/div[2]/p[1]')
+    MESSAGE_NAME_BOOK = (By.XPATH, '/html/body/div[2]/div/div[1]/div[1]/div/strong')
+    BASKET_PRICE = (By.XPATH, '/html/body/div[2]/div/div[1]/div[3]/div/p[1]/strong')
