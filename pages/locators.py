d2ead5b19a56b1c06bbe6d33072772b748cebcf6
Nikita Yuzva <yuzva71@mail.ru>
2023-03-05 03:17:07 +0200

    Add login link

--- pages/locators.py
+++ pages/locators.py
@@ -13,3 +13,8 @@ class ProductPageLocators():
     PRICE_BOOK = (By.XPATH, '/html/body/div[2]/div/div[2]/div[2]/article/div[1]/div[2]/p[1]')
     MESSAGE_NAME_BOOK = (By.XPATH, '/html/body/div[2]/div/div[1]/div[1]/div/strong')
     BASKET_PRICE = (By.XPATH, '/html/body/div[2]/div/div[1]/div[3]/div/p[1]/strong')
+    SUCCESS_MESSAGE = (By.CLASS_NAME, 'alertinner')
+
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
